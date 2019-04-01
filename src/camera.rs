fcd2659219c76346ea6afd8db3f539ecda29cbef
Frederic Merizen <frederic.merizen@gmail.com>
2019-04-01 22:05:52 +0200

    Chapter 6 - camera

---
+++ src/camera.rs
@@ -0,0 +1,24 @@
+use crate::vec3::{Dir, Float, Pos};
+use crate::ray::Ray;
+
+pub struct Camera {
+    lower_left_corner: Dir,
+    horizontal: Dir,
+    vertical: Dir,
+    origin: Pos,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        Camera {
+            lower_left_corner: Dir::new(-2., -1., -1.),
+            horizontal: Dir::new(4., 0., 0.),
+            vertical: Dir::new(0., 2., 0.),
+            origin: Pos::new(0., 0., 0.),
+        }
+    }
+
+    pub fn get_ray(&self, u: Float, v: Float) -> Ray {
+        Ray::new(self.origin, self.lower_left_corner + u * self.horizontal + v * self.vertical)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+mod camera;
 mod hitable;
 mod ray;
 mod vec3;
 
+use camera::Camera;
 use hitable::{HitRecord, Hitable, HitableList, Sphere};
 use ray::Ray;
-use vec3::{Col, Dir, Float, Pos, Vector};
+use vec3::{Col, Float, Pos, Vector};
 
 fn colour(r: &Ray, hitable: &Hitable) -> Col {
     if let Some(HitRecord { normal, .. }) = hitable.hit(r, 0., std::f32::MAX) {
@@ -34,18 +36,15 @@ fn as_u8(f: Float) -> u8 {
 fn main() {
     let nx = 200u16;
     let ny = 100u16;
+    let cam = Camera::new();
     let world = world();
 
     print!("P3\n{} {}\n255\n", nx, ny);
-    let lower_left_corner = Dir::new(-2., -1., -1.);
-    let horizontal = Dir::new(4., 0., 0.);
-    let vertical = Dir::new(0., 2., 0.);
-    let origin = Pos::new(0., 0., 0.);
     for j in (0..ny).rev() {
         for i in 0..nx {
             let u = Float::from(i) / Float::from(nx);
             let v = Float::from(j) / Float::from(ny);
-            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+            let r = cam.get_ray(u,v);
             let col = colour(&r, &world);
 
             let ir = as_u8(col.r());
