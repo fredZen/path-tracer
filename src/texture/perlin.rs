4c02bcc277d62adb4e928c744b8d78f8a4e199d9
Frederic Merizen <frederic@merizen.org>
2019-04-25 20:14:30 +0200

    Book 2 chapter 4 - trilinear filtered perlin noise

--- src/texture/perlin.rs
+++ src/texture/perlin.rs
@@ -23,25 +23,73 @@ lazy_static! {
     };
 }
 
+#[inline]
+pub fn trilinear_interp(c: [[[Float; 2]; 2]; 2], u: Float, v: Float, w: Float) -> Float {
+    let mut accum = 0.;
+
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                let mut c = c[i][j][k];
+
+                let i = i as Float;
+                let j = j as Float;
+                let k = k as Float;
+
+                c *= i * u + (1. - i) * (1. - u);
+                c *= j * v + (1. - j) * (1. - v);
+                c *= k * w + (1. - k) * (1. - w);
+
+                accum += c;
+            }
+        }
+    }
+
+    accum
+}
+
 pub fn noise(p: Pos) -> Float {
-    let u = p.x() - p.x().floor();
-    let v = p.y() - p.y().floor();
-    let w = p.z() - p.z().floor();
+    let i = p.x().floor();
+    let j = p.y().floor();
+    let k = p.z().floor();
 
-    let i = ((4. * p.x()) as i64 & 255) as usize;
-    let j = ((4. * p.y()) as i64 & 255) as usize;
-    let k = ((4. * p.z()) as i64 & 255) as usize;
+    let u = p.x() - i;
+    let v = p.y() - j;
+    let w = p.z() - k;
 
-    let PerlinData{ranfloat, perm_x, perm_y, perm_z} = &*PERLIN_DATA;
+    let i = i as usize;
+    let j = j as usize;
+    let k = k as usize;
 
-    ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]]
+    let mut c = [[[0 as Float; 2]; 2]; 2];
+
+    let PerlinData {
+        ranfloat,
+        perm_x,
+        perm_y,
+        perm_z,
+    } = &*PERLIN_DATA;
+
+    for di in 0..2 {
+        for dj in 0..2 {
+            for dk in 0..2 {
+                let px = perm_x[(i + di) & 255];
+                let py = perm_y[(j + dj) & 255];
+                let pz = perm_z[(k + dk) & 255];
+
+                c[di][dj][dk] = ranfloat[px ^ py ^ pz];
+            }
+        }
+    }
+
+    trilinear_interp(c, u, v, w)
 }
 
 #[derive(Clone, Debug)]
 pub struct NoiseTexture;
 
 impl Texture for NoiseTexture {
-    fn value(&self, u: Float, v: Float, p: Pos) -> Col {
+    fn value(&self, _u: Float, _v: Float, p: Pos) -> Col {
         col(1., 1., 1.) * noise(p)
     }
 }
